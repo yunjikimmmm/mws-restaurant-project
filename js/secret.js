84b3a9fc28ebb86817300e2a525fb9920361c97b
yunjikimmmm <yunjikim@outlook.com>
2020-01-02 17:28:23 -0800

    Enable mapbox maps, but keep API key secret

---
+++ js/secret.js
@@ -0,0 +1,6 @@
+class SECRET {
+  static get mapbox_key() {
+    //return your mapbox API KEY
+    return "pk.eyJ1IjoieXVuamlraW1tbW0iLCJhIjoiY2s0eGJ2b2w0MzE3cDNtcGVjMmFqcGthZSJ9.0S6s_Cup_cBwpBNn7-gOSw";
+  }
+}
